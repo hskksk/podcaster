e91133db95b27dba277f9b40057ee3fd57fdd581
hskksk <hskksk@gmail.com>
2026-09-24 17:18:49 +0900

    Revert "Fall back to SUPABASE_DB_URL when EDGE_WORKER_DB_URL is unset (#118)" (#123)
    This reverts commit 934c944a24b66d492181922e28b5210da85ef86a.

--- supabase/functions/craft-episode-download-worker/index.ts
+++ supabase/functions/craft-episode-download-worker/index.ts
@@ -1,5 +1,4 @@
 import { EdgeWorker } from "@pgflow/edge-worker";
 import { CraftEpisodeDownload } from "../../flows/craft-episode-download.ts";
-import { workerConnectionString } from "../_shared/worker-connection.ts";
 
-EdgeWorker.start(CraftEpisodeDownload, { connectionString: workerConnectionString() });
+EdgeWorker.start(CraftEpisodeDownload);
